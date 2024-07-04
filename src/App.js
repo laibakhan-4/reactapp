8e1ee3f5dd299d0d33005275a1e9acf28c763cf9
laiba-khan4 <laibakhan.intern@devsinc.com>
2024-07-04 20:45:42 +0500

    Learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
-function App() {
+
+const App=()=> {
+
+  const [counter,setCounter]=useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button>-</button>
+      <h1>0</h1>
+      <button>+</button>
+    </div> 
   );
 }
 
